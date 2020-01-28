665ea6b400c7e81c008c98df786985ff4338bcf7
Rezwan Kader <rezwan.kaderb80cse.uap@gmail.com>
2020-01-28 22:30:19 +0600

    By Rezwan Kader

--- E_Urbanization/settings.py
+++ E_Urbanization/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'eurban.apps.EurbanConfig'
     'bootstrap4'
 ]
 
---
+++ eurban/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EurbanConfig(AppConfig):
+    name = 'eurban'
